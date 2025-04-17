a7d6bac19f70d2f59faf9f07c1e2ad7ce00c1c7e
bigpimplimp <levi.jcecil@gmail.com>
2025-04-16 18:03:11 -0700

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -13,15 +13,21 @@ export const getDate = function getDate(result) {
 
 const addDate = function addDate(days) {
     const gridContainer = document.querySelectorAll('.grid-item');
+    const currrentTemp = document.querySelectorAll('.current-temp');
     const gridItem = [...gridContainer];
+    console.log(gridItem)
     let i = 0
     gridItem.some((e) => {
-        e.firstElementChild.children[0].innerHTML = days[i].datetime;
-        e.lastElementChild.children[0].innerHTML = days[i].temp;
-        e.lastElementChild.children[1].innerHTML = days[i].tempmax;
-        e.lastElementChild.children[2].innerHTML = days[i].tempmin;
-        e.lastElementChild.children[3].innerHTML = days[i].precipprob;
-        i + 1;
+        e.firstElementChild.children[0].children[0].innerHTML = days[i].datetime;
+        e.firstElementChild.children[0].children[1].innerHTML = days[i].temp;
+        e.firstElementChild.children[0].children[2].innerHTML = days[i].conditions;
+        e.lastElementChild.children[0].children[1].innerHTML = `${days[i].tempmax} / ${days[i].tempmin}`;
+        e.lastElementChild.children[1].children[1].innerHTML = `${days[i].precipprob}%`;
+        e.lastElementChild.children[2].children[1].innerHTML = `${days[i].humidity}%`;
+        e.lastElementChild.children[3].children[1].innerHTML = `${days[i].visibility} mi`
+        e.lastElementChild.children[4].children[1].innerHTML = `${days[i].windspeed} mph`;
+        i++;
+        console.log(i)
     })
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ console.log('test');
 
 export const getData = async function getData(location) {
   try {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S8PT6R6SWQ2SNK6EUXGTYJKGD&include=current,days&elements=datetime,temp,tempmin,tempmax,precipprob,windspeed,conditions,icon`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S8PT6R6SWQ2SNK6EUXGTYJKGD&include=current,days&elements=datetime,conditions,temp,tempmin,tempmax,humidity,precipprob,windspeed,visibility,conditions,icon`);
     const data = await response.json();
     return data;
   } catch {
-    console.log('Did not work');
+    alert('Not a valid location!');
+    return;
   } 
 }
   
@@ -18,8 +19,11 @@ const input = document.querySelector('#search-bar');
     if(e.target.matches('#submit')) {
         getData(input.value)
           .then(result => {
-            console.log(result)
             getDate(result.days)
+          })
+          .catch(error => {
+            console.log(error)
+            return;
           })
         input.value = '';
         e.preventDefault();
