5a63a8b97e3a5983e982893dc14c5f509684f6a7
bigpimplimp <levi.jcecil@gmail.com>
2025-04-28 20:07:35 -0700

    ran pretty

--- src/dom.js
+++ src/dom.js
@@ -1,77 +1,79 @@
 const icons = {
-    'partly-cloudy-day': () => import('./svg/cloudy-1-day.svg'),
-    'rain': () => import('./svg/rainy-3.svg'),
-    'cloudy': () => import('./svg/cloudy.svg'),
-    'clear-day': () => import('./svg/clear-day.svg'),
-    'snow': () => import('./svg/snowy-3.svg')
-}
+  "partly-cloudy-day": () => import("./svg/cloudy-1-day.svg"),
+  rain: () => import("./svg/rainy-3.svg"),
+  cloudy: () => import("./svg/cloudy.svg"),
+  "clear-day": () => import("./svg/clear-day.svg"),
+  snow: () => import("./svg/snowy-3.svg"),
+};
 
 export const unhideGrid = function unhideGrid() {
-    document.getElementById('grid-container').style.visibility = 'visible';
-}
+  document.getElementById("grid-container").style.visibility = "visible";
+};
 
-const loadSVG = async function(key) {
-    try {
-        const module = await icons[key]();
-        return module.default;
-    } catch (error) {
-        console.log('SVGs did not load');
-        return null;
-    }
-}
+const loadSVG = async function (key) {
+  try {
+    const module = await icons[key]();
+    return module.default;
+  } catch (error) {
+    console.log("SVGs did not load");
+    return null;
+  }
+};
 
 export const updateDom = async function updateDom(days) {
-    const gridContainer = document.querySelectorAll('.grid-item');
-    const gridItem = [...gridContainer];
-    
-    for (let i = 0; i < gridItem.length; i++) {
-        const e = gridItem[i];
+  const gridContainer = document.querySelectorAll(".grid-item");
+  const gridItem = [...gridContainer];
 
-        e.firstElementChild.children[0].children[0].innerHTML = convertDate(days[i].datetime);
-        e.firstElementChild.children[0].children[1].innerHTML = `${days[i].temp}\u00B0F`;
-        e.firstElementChild.children[0].children[2].innerHTML = days[i].conditions;
-        e.firstElementChild.children[1].firstElementChild.src = await determineIcon(days[i].icon);
-        e.lastElementChild.children[0].children[1].innerHTML = `${days[i].tempmax}\u00B0F / ${days[i].tempmin}\u00B0F`;
-        e.lastElementChild.children[1].children[1].innerHTML = `${days[i].precipprob}%`;
-        e.lastElementChild.children[2].children[1].innerHTML = `${days[i].humidity}%`;
-        e.lastElementChild.children[3].children[1].innerHTML = `${days[i].visibility} mi`
-        e.lastElementChild.children[4].children[1].innerHTML = `${days[i].windspeed} mph`;
-    }
-}
+  for (let i = 0; i < gridItem.length; i++) {
+    const e = gridItem[i];
+
+    e.firstElementChild.children[0].children[0].innerHTML = convertDate(
+      days[i].datetime,
+    );
+    e.firstElementChild.children[0].children[1].innerHTML = `${days[i].temp}\u00B0F`;
+    e.firstElementChild.children[0].children[2].innerHTML = days[i].conditions;
+    e.firstElementChild.children[1].firstElementChild.src = await determineIcon(
+      days[i].icon,
+    );
+    e.lastElementChild.children[0].children[1].innerHTML = `${days[i].tempmax}\u00B0F / ${days[i].tempmin}\u00B0F`;
+    e.lastElementChild.children[1].children[1].innerHTML = `${days[i].precipprob}%`;
+    e.lastElementChild.children[2].children[1].innerHTML = `${days[i].humidity}%`;
+    e.lastElementChild.children[3].children[1].innerHTML = `${days[i].visibility} mi`;
+    e.lastElementChild.children[4].children[1].innerHTML = `${days[i].windspeed} mph`;
+  }
+};
 
 const convertDate = function convertDate(value) {
-    const date = new Date(value + 'T00:00:00');
-    const options = { 
-        weekday: 'long',
-        month: 'numeric',
-        day: 'numeric'
-    };
-    return (Intl.DateTimeFormat('en-US',
-        options).format(date));
-  }
+  const date = new Date(value + "T00:00:00");
+  const options = {
+    weekday: "long",
+    month: "numeric",
+    day: "numeric",
+  };
+  return Intl.DateTimeFormat("en-US", options).format(date);
+};
 
 const determineIcon = async function determineIcon(value) {
-    if (icons[value]) {
-        return await loadSVG(value);
-    } else {
-        console.log(`Unknown icon value ${value}`)
-    }
-}
+  if (icons[value]) {
+    return await loadSVG(value);
+  } else {
+    console.log(`Unknown icon value ${value}`);
+  }
+};
 
 export const updateLocation = function updateLocation(value) {
-    const e = document.getElementById('location');
-    e.innerHTML = `${value}`
-}
+  const e = document.getElementById("location");
+  e.innerHTML = `${value}`;
+};
 
 export const updateTemp = function updateTemp(arr, symbol) {
-    const gridContainer = document.querySelectorAll('.grid-item');
-    const gridItem = [...gridContainer];
+  const gridContainer = document.querySelectorAll(".grid-item");
+  const gridItem = [...gridContainer];
 
-    for (let i = 0; i < gridItem.length; i++) {
-        const e = gridItem[i];
+  for (let i = 0; i < gridItem.length; i++) {
+    const e = gridItem[i];
 
-        e.firstElementChild.children[0].children[1].innerHTML = `${arr[i].temp}\u00B0${symbol}`;
-        e.lastElementChild.children[0].children[1].innerHTML = `${arr[i].tempmax}\u00B0${symbol} / ${arr[i].tempmin}\u00B0${symbol}`;
-
-    }
-}
+    e.firstElementChild.children[0].children[1].innerHTML = `${arr[i].temp}\u00B0${symbol}`;
+    e.lastElementChild.children[0].children[1].innerHTML = `${arr[i].tempmax}\u00B0${symbol} / ${arr[i].tempmin}\u00B0${symbol}`;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,90 +1,90 @@
-import './style.css';
-import { updateDom, unhideGrid, updateLocation, updateTemp } from './dom.js';
-console.log('test');
+import "./style.css";
+import { updateDom, unhideGrid, updateLocation, updateTemp } from "./dom.js";
+console.log("test");
 
 export const getData = async function getData(location) {
   try {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S8PT6R6SWQ2SNK6EUXGTYJKGD&include=current,days&elements=datetime,conditions,temp,tempmin,tempmax,humidity,precipprob,windspeed,visibility,conditions,icon`);
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=S8PT6R6SWQ2SNK6EUXGTYJKGD&include=current,days&elements=datetime,conditions,temp,tempmin,tempmax,humidity,precipprob,windspeed,visibility,conditions,icon`,
+    );
     const data = await response.json();
     console.log(data);
     return data;
   } catch {
-    alert('Error fetching data');
+    alert("Error fetching data");
     return;
-  } 
-}
-  
-const form = document.querySelector('#form');
-const input = document.querySelector('#search-bar');
-const checkbox = document.querySelector('#checkbox');
-  form.addEventListener('click', (e) => {
-    if(e.target.matches('#submit')) {
-        getData(input.value)
-          .then(result => {
-            updateLocation(result.resolvedAddress)
-            updateDom(result.days)
-            unhideGrid()
-            storeItem('temps', result.days)
-          })
-          .catch(error => {
-            console.log(error)
-            return;
-          })
-        input.value = '';
-        e.preventDefault();
-    }
-  })
-  checkbox.addEventListener('click', (e) => {
-    convertDegree();
+  }
+};
 
-  })
+const form = document.querySelector("#form");
+const input = document.querySelector("#search-bar");
+const checkbox = document.querySelector("#checkbox");
+form.addEventListener("click", (e) => {
+  if (e.target.matches("#submit")) {
+    getData(input.value)
+      .then((result) => {
+        updateLocation(result.resolvedAddress);
+        updateDom(result.days);
+        unhideGrid();
+        storeItem("temps", result.days);
+      })
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
+    input.value = "";
+    e.preventDefault();
+  }
+});
+checkbox.addEventListener("click", (e) => {
+  convertDegree();
+});
 
 const returnCelcius = function convert(value) {
-
-    let x = (5 / 9) * (value - 32);
-    return String(x).slice(0, 4)
-}  
+  let x = (5 / 9) * (value - 32);
+  return String(x).slice(0, 4);
+};
 
 const convertDegree = function convertDegree(value) {
-    const isActive = checkbox.checked;
+  const isActive = checkbox.checked;
 
-    if (isActive) {
-      let newArr = [];
-      const arr = retrieveItem('temps')
-      console.log('break')
-      arr.forEach(e => {
-        console.log('test')
-          let obj = { 
-            tempmax: returnCelcius(e.tempmax),
-            tempmin: returnCelcius(e.tempmin),
-            temp: returnCelcius(e.temp)
-          }
-          newArr.push(obj);
-      });
-      console.log(newArr)
-      updateTemp(newArr, 'C')
-      storeItem('celc', newArr)
-    }
-    if (!isActive) {
-      let newArr = []
-      const arr = retrieveItem('temps')
-      arr.forEach(e => {
-        let obj = {
-          tempmax: (e.tempmax),
-          tempmin: (e.tempmin),
-          temp: (e.temp)
-        }
-        newArr.push(obj);
-      })
-      updateTemp(newArr, 'F')
-      console.log(newArr)
-    }
-}
+  if (isActive) {
+    let newArr = [];
+    const arr = retrieveItem("temps");
+    console.log("break");
+    arr.forEach((e) => {
+      console.log("test");
+      let obj = {
+        tempmax: returnCelcius(e.tempmax),
+        tempmin: returnCelcius(e.tempmin),
+        temp: returnCelcius(e.temp),
+      };
+      newArr.push(obj);
+    });
+    console.log(newArr);
+    updateTemp(newArr, "C");
+    storeItem("celc", newArr);
+  }
+  if (!isActive) {
+    let newArr = [];
+    const arr = retrieveItem("temps");
+    arr.forEach((e) => {
+      let obj = {
+        tempmax: e.tempmax,
+        tempmin: e.tempmin,
+        temp: e.temp,
+      };
+      newArr.push(obj);
+    });
+    updateTemp(newArr, "F");
+    console.log(newArr);
+  }
+};
 
 const storeItem = function storeItem(key, value) {
-    localStorage.setItem(key, JSON.stringify(value))
-}
+  localStorage.setItem(key, JSON.stringify(value));
+};
 
 const retrieveItem = function retrieveItem(value) {
-    return JSON.parse(localStorage.getItem(value))
-}
+  return JSON.parse(localStorage.getItem(value));
+};
